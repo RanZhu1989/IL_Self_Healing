6e221c0ee92cc6bab9a24404721e0d233b62308f
Ran Zhu <zhura@tcd.ie>
2024-07-16 10:17:21 +0100

    Reorganize miscellaneous files

--- misc/HowToPyJulia/jtest.jl
+++
@@ -1,7 +0,0 @@
-function foo(x)
-    if x > 0
-        return x * 2 ,x*6
-    else
-        return x*3, x*4, x*5
-    end
-end
--- misc/HowToPyJulia/test.py
+++
@@ -1,9 +0,0 @@
-from juliacall import Main as jl
-
-jl.include("jtest.jl") # 求解优化问题，并打印问题
-
-# 多态函数
-x = jl.foo(-1)
-
-a,b,c = x
-print(a,b,c)
